2cf4ad32ef4606f90c51d1808b85cb81fd5649f8
Chaudhari <2478201@cognizant.com>
2026-03-05 14:46:07 +0530

    Commit 2

--- src/main/java/com/cts/agrichain/AgrichainApplication.java
+++ src/main/java/com/cts/agrichain/AgrichainApplication.java
@@ -10,6 +10,7 @@ public class AgrichainApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AgrichainApplication.class, args);
 		ApplicationContext context = SpringApplication.run(AgrichainApplication.class, args);
+		System.out.println("dfghjk");
 	}
 
 }

729294daa18409f77c60d5cf053fdcf0f89dbcd6
Thanus Thapasvi <ThanusThapasvi.T@cognizant.com>
2026-03-10 17:39:07 +0530

    Disbursement entity

--- src/main/java/com/cts/agrichain/entity/Disbursement.java
+++ src/main/java/com/cts/agrichain/entity/Disbursement.java
@@ -1,14 +1,65 @@
 package com.cts.agrichain.entity;
 
 import com.cts.agrichain.enums.DisbursementStatus;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
 
 import java.time.LocalDate;
 
+@Entity
 public class Disbursement {
+
+    @Id
     private int DisbursementID;
     private int FarmerID;
     private int ProgramID;
     private double disbursementAmount;
     private LocalDate disbursementDate;
+
+    @Enumerated(EnumType.STRING)
     private DisbursementStatus disbursementStatus;
+
+    public int getDisbursementID() {
+        return DisbursementID;
+    }
+    public void setDisbursementID(int disbursementID) {
+        DisbursementID = disbursementID;
+    }
+
+    public int getFarmerID() {
+        return FarmerID;
+    }
+    public void setFarmerID(int farmerID) {
+        FarmerID = farmerID;
+    }
+
+    public int getProgramID() {
+        return ProgramID;
+    }
+    public void setProgramID(int programID) {
+        ProgramID = programID;
+    }
+
+    public double getDisbursementAmount() {
+        return disbursementAmount;
+    }
+    public void setDisbursementAmount(double disbursementAmount) {
+        this.disbursementAmount = disbursementAmount;
+    }
+
+    public LocalDate getDisbursementDate() {
+        return disbursementDate;
+    }
+    public void setDisbursementDate(LocalDate disbursementDate) {
+        this.disbursementDate = disbursementDate;
+    }
+
+    public DisbursementStatus getDisbursementStatus() {
+        return disbursementStatus;
+    }
+    public void setDisbursementStatus(DisbursementStatus disbursementStatus) {
+        this.disbursementStatus = disbursementStatus;
+    }
 }
